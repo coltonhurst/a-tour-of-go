5895c05e2ff67b2ec67efcc69d0be6b2be9e0e30
coltonhurst <colton@coltonhurst.com>
2019-05-24 17:55:05 -0400

    Add ex 2.9

---
+++ methods-and-interfaces/2.9_interfaces.go
@@ -0,0 +1,50 @@
+/*
+	Notes 2.9
+
+	An interface type is defined as a set of method signatures.
+
+	A value of interface type can hold any value that implements those methods.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+type Abser interface {
+	Abs() float64
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f	// a MyFloat implements Abser
+	a = &v	// a *Vertex implements Abser
+
+	// In the following line, v is a Vertex (not *Vertex)
+	// and does NOT implement Abser.
+	a = v	// error on this line
+
+	fmt.Println(a.Abs())
+}
