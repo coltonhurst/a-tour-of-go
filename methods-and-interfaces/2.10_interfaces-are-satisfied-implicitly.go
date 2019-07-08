38be4255a000d38e26797aa79055a3bc6252ddaf
coltonhurst <colton@coltonhurst.com>
2019-07-08 13:46:28 -0400

    Add 2.10 and 2.11 examples

---
+++ methods-and-interfaces/2.10_interfaces-are-satisfied-implicitly.go
@@ -0,0 +1,28 @@
+/*
+	Notes 2.10
+
+	You don't need to be explicity about a type that implements an interface.
+*/
+
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+}
---
+++ methods-and-interfaces/2.11_interface-values.go
@@ -0,0 +1,53 @@
+/*
+	Notes 2.11
+
+	"Interface values can be thought of as a tuple of a value and a concrete type. (value, type)
+	An interface value holds a value of a specific underlying concrete type.
+	Calling a method on an interface value executes the method of the same name on its underlying type."
+
+
+	fmt.Printf verbs:
+		%v = the value in a default format
+		%T = a Go-syntax representation of the type of the value
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
+func main() {
+	var i I
+
+	i = &T{"hello"}
+	describe(i)
+	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
