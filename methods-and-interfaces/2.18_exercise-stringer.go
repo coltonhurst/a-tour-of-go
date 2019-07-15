154f2b805dfcfe2df9f81570c4ca1766b1b15b32
coltonhurst <colton@coltonhurst.com>
2019-07-15 12:01:44 -0400

    Add through 2.19

---
+++ methods-and-interfaces/2.18_exercise-stringer.go
@@ -0,0 +1,25 @@
+/*
+	Notes 2.18
+
+	The exercise: "Make the IPAddr type implement fmt.Stringer to print the address as a dotted quad."
+*/
+
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
---
+++ methods-and-interfaces/2.19_errors.go
@@ -0,0 +1,39 @@
+/*
+	Notes 2.19
+
+	"Go programs express error state with error values."
+	"The error type is a built-in interface":
+
+	type error interface {
+    	Error() string
+	}
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+	return &MyError {
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
